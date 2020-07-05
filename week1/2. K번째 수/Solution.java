c13d56c3de0d1841b73ccb6f39cfe42392593491
waterSpray <whdgns5059@gmail.com>
2020-07-06 00:54:26 +0900

    k번째 수 풀이

--- week1/2. K번째 수/Solution.java
+++ week1/2. K번째 수/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
     2. K번째 수 
     https://programmers.co.kr/learn/courses/30/lessons/42748
@@ -5,7 +7,25 @@
 class Solution {
 
     public int[] solution(int[] array, int[][] commands) {
-        return null;
+
+        int[] resultArray = new int[commands.length];
+
+        int index = 0;
+        for (int[] command : commands) {
+
+            int i = command[0] - 1;
+            int j = command[1];
+            int k = command[2] - 1;
+
+            int[] slicedArray = Arrays.copyOfRange(array, i, j);
+
+            Arrays.sort(slicedArray);
+
+            resultArray[index++] = slicedArray[k];
+            ;
+        }
+
+        return resultArray;
     }
 
 }
