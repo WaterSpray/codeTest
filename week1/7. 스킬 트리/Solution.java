58882d26303ad0325e265246942b84df78317f5e
mordiumaco <mordiumaco@gmail.com>
2020-07-14 00:22:52 +0900

    1-7 skill tree

--- week1/7. 스킬 트리/Solution.java
+++ week1/7. 스킬 트리/Solution.java
@@ -1,10 +1,62 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 /*
     7. 스킬 트리
     https://programmers.co.kr/learn/courses/30/lessons/49993
 */
-class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        return answer;
+class Solution7 {
+
+    public static void main(String[] args) {
+
+        String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDA" };
+
+        System.out.println(solution("CBD", skill_trees));
+    }
+
+    public static int solution(String skill, String[] skill_trees) {
+
+        String[] formalSkillTree = skill.split("");
+        Set<String> skillTreeSet = new HashSet<String>();
+
+        for (String skillOrder : formalSkillTree) {
+            skillTreeSet.add(skillOrder);
+        }
+
+        int formalSkillTreeCount = 0;
+
+        for (String notFormalSkillTree : skill_trees) {
+
+            boolean isFormal = true;
+
+            Queue<String> q = new LinkedList<String>(Arrays.asList(formalSkillTree));
+
+            for (String notFormalSkillOrder : notFormalSkillTree.split("")) {
+
+                if (skillTreeSet.contains(notFormalSkillOrder)) {
+
+                    if (notFormalSkillOrder.equals(q.peek())) {
+
+                        q.poll();
+
+                    } else {
+
+                        isFormal = false;
+                        break;
+
+                    }
+
+                }
+
+            }
+
+            if (isFormal)
+                formalSkillTreeCount++;
+        }
+
+        return formalSkillTreeCount;
     }
 }
