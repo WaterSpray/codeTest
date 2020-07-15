0ed1f6aafd51023aff98b9c7ec88fb371432772c
waterSpray <whdgns5059@gmail.com>
2020-07-16 03:30:28 +0900

    방문길이

--- week1/6. 방문 길이/Solution.java
+++ week1/6. 방문 길이/Solution.java
@@ -7,4 +7,54 @@ class Solution {
         int answer = 0;
         return answer;
     }
+
+    class Character {
+
+        int x;
+        int y;
+
+        void up() {
+            if (x < 5) {
+                x += 1;
+            }
+        }
+
+        void down() {
+            if (x > -5) {
+                x -= 1;
+            }
+        }
+
+        void right() {
+            if (y < 5) {
+                y += 1;
+            }
+        }
+
+        void left() {
+            if (y > -5) {
+                y -= 1;
+            }
+        }
+
+        Character move(String command) {
+            switch (command) {
+                case U:
+                    up();
+                    break;
+                case D:
+                    down();
+                    break;
+                case R:
+                    right();
+                    break;
+                case L:
+                    left();
+                    break;
+            }
+            return this;
+        }
+
+    }
+
 }
