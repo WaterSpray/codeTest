1d550631473a782da1691b0969465c03eab2f0bb
waterSpray <whdgns5059@gmail.com>
2020-07-16 03:33:14 +0900

    방문길이

--- week1/6. 방문 길이/Solution.java
+++ week1/6. 방문 길이/Solution.java
@@ -1,60 +1,107 @@
+import java.util.*;
+
 /*
     6. 방문 길이
     https://programmers.co.kr/learn/courses/30/lessons/49994
 */
-class Solution {
-    public int solution(String dirs) {
-        int answer = 0;
-        return answer;
-    }
+public class Solution {
 
-    class Character {
-
+    class Point {
         int x;
         int y;
 
-        void up() {
-            if (x < 5) {
-                x += 1;
-            }
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        void down() {
-            if (x > -5) {
-                x -= 1;
-            }
+        public Point moved(int dx, int dy) {
+            return new Point(x + dx, y + dy);
         }
 
-        void right() {
-            if (y < 5) {
-                y += 1;
-            }
+        public boolean isAvailable() {
+            return -5 <= x && x <= 5 && -5 <= y && y <= 5;
         }
 
-        void left() {
-            if (y > -5) {
-                y -= 1;
-            }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
         }
 
-        Character move(String command) {
-            switch (command) {
-                case U:
-                    up();
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    class Path {
+
+        Point start;
+        Point end;
+
+        public Path(Point start, Point end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            Path path = (Path) o;
+            return start.equals(path.start) && end.equals(path.end) || start.equals(path.end) && end.equals(path.start);
+        }
+
+        @Override
+        public int hashCode() {
+            return start.hashCode() + end.hashCode();
+        }
+    }
+
+    public int solution(String dirs) {
+
+        Point start = new Point(0, 0);
+
+        Set<Path> route = new HashSet<>();
+
+        for (char d : dirs.toCharArray()) {
+
+            Point end = null;
+
+            switch (d) {
+                case 'U':
+                    end = start.moved(0, 1);
                     break;
-                case D:
-                    down();
+                case 'D':
+                    end = start.moved(0, -1);
                     break;
-                case R:
-                    right();
+                case 'L':
+                    end = start.moved(-1, 0);
                     break;
-                case L:
-                    left();
+                case 'R':
+                    end = start.moved(1, 0);
                     break;
             }
-            return this;
+
+            if (end == null)
+                continue;
+            ;
+            if (!end.isAvailable())
+                continue;
+
+            route.add(new Path(start, end));
+            start = end;
+
         }
 
+        return route.size();
     }
 
 }
