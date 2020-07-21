9f4d4db7c0cb52af99e74f27bea16ebb8d21514a
waterSpray <whdgns5059@gmail.com>
2020-07-22 00:58:56 +0900

    2-2 올바른 괄호

--- week2/2. 올바른 괄호/Solution.java
+++ week2/2. 올바른 괄호/Solution.java
@@ -3,5 +3,19 @@
     https://programmers.co.kr/learn/courses/30/lessons/12909
 */
 public class Solution {
+    boolean solution(String s) {
 
+        int counter = 0;
+        for (Character bracket : s.toCharArray()) {
+            if (bracket == '(') {
+                counter++;
+            } else {
+                if (counter == 0) {
+                    return false;
+                }
+                counter--;
+            }
+        }
+        return counter == 0;
+    }
 }
