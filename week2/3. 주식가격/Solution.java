7a19cbe7273da5201669001537f81d73f59c6c3d
waterSpray <whdgns5059@gmail.com>
2020-07-29 00:07:34 +0900

    주식가격

--- week2/3. 주식가격/Solution.java
+++ week2/3. 주식가격/Solution.java
@@ -3,5 +3,27 @@
     https://programmers.co.kr/learn/courses/30/lessons/42584
 */
 public class Solution {
+    public int[] solution(int[] prices) {
 
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = countPriceTerm(i, prices);
+        }
+        return answer;
+    }
+
+    private int countPriceTerm(int index, int[] prices) {
+
+        int price = prices[index];
+        int count = 0;
+
+        for (int i = index + 1; i < prices.length; i++) {
+            count++;
+            if (price > prices[i]) {
+                break;
+            }
+        }
+        return count;
+    }
 }
